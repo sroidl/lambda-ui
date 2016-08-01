a0e3e52cba793cbc0d27c38b83db960f9f531e86
Hermann Vocke <hvocke@thoughtworks.com>
2016-08-01 09:25:22 +0200

    add dynamic css class to build bar

--- src/BuildBar.js
+++ src/BuildBar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './BuildBar.css';
+import classNames from 'classnames';
 
 class BuildBar extends Component {
   render() {
+    let classes = classNames("buildSummary", this.props.state);
+
     return (
-      <div className="buildSummary">
+      <div className={classes}>
         <div className="buildNumber">
           #{this.props.buildNumber}: {this.props.state}
         </div>
