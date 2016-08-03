1874b85b39489c63319a3ad59003507992b25ae0
Hermann Vocke <hvocke@thoughtworks.com>
2016-08-03 08:51:16 +0200

    add icon fonts

--- src/BuildBar.js
+++ src/BuildBar.js
@@ -6,9 +6,11 @@ class BuildBar extends Component {
   render() {
     let classes = classNames("buildSummary", this.props.state);
 
+    let buildIcon = this.getBuildIcon(this.props.state);
+
     return (
       <div className={classes}>
-        <span className="buildIcon">?</span>
+        <span className="buildIcon">{buildIcon}</span>
         <span className="buildNumber">
           #{this.props.buildNumber}: {this.props.state}
         </span>
@@ -16,6 +18,21 @@ class BuildBar extends Component {
         <span className="duration">Duration: 5min 46sec</span>
       </div>
     );
+  }
+
+  getBuildIcon(state) {
+    switch(state) {
+      case "success":
+        return <i className='fa fa-check' aria-hidden='true'></i>;
+      case "inProgress":
+        return <i className='fa fa-cog' aria-hidden='true'></i>;
+      case "error":
+      default:
+        return <i className='fa fa-exclamation' aria-hidden='true'></i>;
+
+
+    }
+
   }
 }
 
