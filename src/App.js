81e7c565f2a8687f30918e3a9b7c2a74625ffa68
Hermann Vocke <hvocke@thoughtworks.com>
2016-08-03 08:36:34 +0200

    tweak header styling, add web font

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>LambdaCD</h2>
+          <h2 className="LogoText">LambdaCD</h2>
         </div>
         <div className="App-intro">
           <BuildList data={data} />
