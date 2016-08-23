2d6afbdde2170bc1757eceb7267c2be0dd30def1
Sebastian Roidl <sroidl@thoughtworks.com>
2016-08-24 01:11:39 +0200

    Add CORS header. Add buildNumber attribute to BuildSummaries

--- src/lambdaui/dummy_data.clj
+++ src/lambdaui/dummy_data.clj
@@ -1,7 +1,8 @@
 (ns lambdaui.dummy-data
-  (:require [ring.util.response :refer [response]]))
+  (:require [ring.util.response :refer [response header]]))
 
 (def summaries {:summaries [{:buildId   1
+                             :buildNumber 1                 ; TODO differentiate between build Number (alias) and id?
                              :state     :running
                              :startTime "11pm"              ; TODO decide on format (ISO timestamp?)
                              :duration  "30 seconds"        ; TODO decide on format (maybe seconds?)
@@ -13,5 +14,5 @@
 
 
 (defn hello-response [] (response "haallo welt"))
-(defn build-summaries [] (response summaries))
+(defn build-summaries [] (header (response summaries) "Access-Control-Allow-Origin" "*"))
 
