d5995c2fa5166fd01f1b88506e65ab5f49c3af4c
Sebastian Roidl <sebastian.roidl@otto.de>
2016-08-24 16:55:47 +0200

    Add Jest testframework

---
+++ resources/ui/src/js/__tests__/AppState-test.js
@@ -0,0 +1,18 @@
+// jest.unmock('../AppState.es6');
+// jest.unmock('redux');
+//
+// import appState from '../AppState.es6'
+
+// console.log(appState)
+
+describe("Example Test", () =>{
+  it("should pass", () =>{
+
+
+
+  })
+
+  it("shoudl not fail", () => {
+    expect(true).not.toBe(false);
+  })
+})
