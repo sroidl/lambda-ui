675afcb5bfd9974de15a2fd08e65bab7fe4f1acf
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-01 09:40:54 +0200

    formatting

--- backend/project.clj
+++ backend/project.clj
@@ -2,19 +2,22 @@
   :description "LambdaCD-Plugin that provides a modern UI for your pipeline."
   :url "https://github.com/sroidl/lambda-ui"
   :license {:name "Apache License 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [lambdacd "0.9.0"]
                  [compojure "1.5.0"]
                  [http-kit "2.1.18"]
                  [org.clojure/data.json "0.2.6"]
                  ]
-  :main lambdaui.core
-  :test-paths ["test" "example"]
+
+  :test-paths ["test"]
+
   :deploy-repositories [["clojars" {:creds :gpg}]
                         ["releases" :clojars]
-			["snapshots" :clojars]]
+                        ["snapshots" :clojars]]
   :profiles {:dev {:dependencies [[lambdacd-git "0.1.2"]
                                   [ring-server "0.4.0"]]
-                   :main lambdaui.core
+
+                   :aot [lambdaui.core]
+                   :main         lambdaui.core
                    }})
