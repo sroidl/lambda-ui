03d00fcefa02ccfa9a6b3bb7421177671121ee6a
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-02 13:20:38 +0200

    formatting

--- example-pipeline/src/lambdaui/example/simple_pipeline.clj
+++ example-pipeline/src/lambdaui/example/simple_pipeline.clj
@@ -1,28 +1,27 @@
 (ns lambdaui.example.simple-pipeline
-    (:use [compojure.core])
-    (:require [lambdacd.steps.shell :as shell]
-      [lambdacd.steps.manualtrigger :refer [wait-for-manual-trigger]]
-      [lambdacd.steps.control-flow :refer [either with-workspace in-parallel run]]
-      [lambdacd.core :as lambdacd]
-      [ring.server.standalone :as ring-server]
-              [lambdacd.ui.api]
-      [lambdacd-git.core :as git]
-      [lambdacd.runners :as runners]
-      [clojure.java.io :as io]
-      [lambdaui.api :as ui]))
+  (:use [compojure.core])
+  (:require [lambdacd.steps.shell :as shell]
+            [lambdacd.steps.manualtrigger :refer [wait-for-manual-trigger]]
+            [lambdacd.steps.control-flow :refer [either with-workspace in-parallel run]]
+            [lambdacd.core :as lambdacd]
+            [ring.server.standalone :as ring-server]
+            [lambdacd.ui.api]
+            [lambdacd-git.core :as git]
+            [lambdacd.runners :as runners]
+            [clojure.java.io :as io]
+            [lambdaui.api :as ui]))
 
 (def repo "git@github.com:flosell/testrepo")
 
-(defn wait-for-git [args ctx]`
-      (git/wait-for-git ctx repo
-                        :ref "refs/heads/master"
-                        :ms-between-polls (* 60 1000)))
+(defn wait-for-git [args ctx] `(git/wait-for-git ctx repo
+                                                 :ref "refs/heads/master"
+                                                 :ms-between-polls (* 60 1000)))
 
 (defn clone [args ctx]
-      (git/clone ctx repo (:revision args) (:cwd args)))
+  (git/clone ctx repo (:revision args) (:cwd args)))
 
 (defn ls [args ctx]
-      (shell/bash ctx (:cwd args) "ls"))
+  (shell/bash ctx (:cwd args) "ls"))
 
 (def pipeline-structure
   `((either
@@ -42,14 +41,20 @@
       default)
     ))
 
+(defonce pipe (atom nil))
+
 (defn -main [& args]
-      (let [home-dir (io/file "/tmp/foo")
-            config   {:home-dir home-dir}
-            port  (try-parse (System/getenv "PORT") 8082)
-            pipeline (lambdacd/assemble-pipeline pipeline-structure config)]
-           (git/init-ssh!)
-           (runners/start-one-run-after-another pipeline)
-           (ring-server/serve (routes
-                                (ui/ui-for-pipeline pipeline))
-                              {:open-browser? false
-                               :port          port})))
+  (let [home-dir (io/file "/tmp/foo")
+        config {:home-dir home-dir}
+        port (try-parse (System/getenv "PORT") 8082)
+        pipeline (lambdacd/assemble-pipeline pipeline-structure config)]
+
+    (reset! pipe pipeline)
+
+
+    (git/init-ssh!)
+    (runners/start-one-run-after-another pipeline)
+    (ring-server/serve (routes
+                         (ui/ui-for-pipeline pipeline))
+                       {:open-browser? false
+                        :port          port})))
