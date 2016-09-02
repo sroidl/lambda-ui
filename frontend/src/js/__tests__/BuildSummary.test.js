0012398237d937e731f342fb74f3262f66c393d3
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-02 13:17:36 +0200

    proper formatting of duration, first try formatting start time

--- frontend/src/js/__tests__/BuildSummary.test.js
+++ frontend/src/js/__tests__/BuildSummary.test.js
@@ -1,14 +1,18 @@
 import ReduxBuildSummary, {BuildSummary} from '../BuildSummary.es6';
 import * as subject from '../BuildSummary.es6'
 import {shallow} from 'enzyme';
+import {now} from 'moment';
 
 function buildIcon(domElement){
   return domElement.find('.buildIcon').find('.fa');
 }
 
+function domDuration(domElement){
+  return domElement.find('.buildDuration');
+}
 
-
-describe("BuildSummary BuildIcons", () =>{
+describe("BuildSummary Display", ()=>{
+  describe("BuildIcons", () =>{
   it("should show correct failed state icon", ()=>{
     let inputProps = {buildId: 1, state: 'failed', startTime: {toISOString: jest.fn()}, toggleBuildDetails: undefined};
 
@@ -33,7 +37,38 @@ describe("BuildSummary BuildIcons", () =>{
     expect(buildIcon(component).hasClass('fa-cog')).toBe(true);
   });
 });
+  describe("Duration", ()=>{
+      const defaultProps = newAttributes =>
+      Object.assign({buildId:1, state:'running', toggleBuildDetails: undefined, startTime: now()}, newAttributes)
 
+      const expectDuration = (input, expectedOutput) => {
+        let inputProps = defaultProps({duration: input})
+
+        let component = shallow(BuildSummary(inputProps));
+
+        let duration = str => "Duration: " + str;
+        expect(domDuration(component).text()).toEqual(duration(expectedOutput))
+      }
+
+      it("should display only seconds if less than one minute", ()=>{
+        expectDuration(2, "2 seconds");
+        expectDuration(59,"59 seconds");
+      })
+      it("should display minutes in mm:ss format if less than one hour", ()=>{
+        expectDuration(60, "1 minute")
+        expectDuration(61, "1 minute 1 second");
+        expectDuration(630, "10 minutes 30 seconds")
+        expectDuration(600, "10 minutes");
+      })
+
+      const hours = int => int * 60 * 60;
+      const minutes = int => int * 60;
+      it("should display hours properly", ()=>{
+        expectDuration(hours(1), "1 hour")
+        expectDuration(hours(2)+ minutes(35) + 45, "2 hours 35 minutes 45 seconds")
+      })
+  })
+})
 describe("BuildSummary Toggle", ()=>{
   it("should call the toggle details function on click", ()=>{
     let toggleFnMock = jest.fn();
