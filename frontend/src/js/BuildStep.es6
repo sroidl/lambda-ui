d4cec4f695e73165edda21543b651050cde66bb5
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-04 21:37:08 +0200

    basic rendering of build steps

--- frontend/src/js/BuildStep.es6
+++ frontend/src/js/BuildStep.es6
@@ -2,12 +2,23 @@ import React from 'react';
 import Moment from 'moment';
 import R from 'ramda';
 import Utils from './ComponentUtils.es6'
+import "moment-duration-format";
+
+
+const duration = ({startTime, endTime}) => {
+  let start = Moment(startTime);
+  let end = Moment(endTime);
+
+  let duration = Moment.duration(end.diff(start), "milliseconds");
+
+  return duration.format("hh:mm:ss")
+}
 
 export const BuildStep = props => {
   let {buildId, step} = props;
 
-  return <div className={Utils.classes("buildStep", step.state)}>
-    <div className="stepId">Id: {step.stepId}</div>
-    <div className="stepName">Name: {step.stepId}</div>
-  </div>
+  return <span className={Utils.classes("buildStep", step.state)}>
+    <div className="stepName">{step.name}</div>
+    <div className="stepDuration">{duration(step)}</div>
+  </span>
 }
--- frontend/src/js/__tests__/BuildStep.test.js
+++ frontend/src/js/__tests__/BuildStep.test.js
@@ -1,7 +1,34 @@
+import React from 'react';
+import {shallow} from 'enzyme';
 import {BuildStep} from '../BuildStep.es6'
 
-describe("BuildStep Component", ()=>{
-  it("should render all step information", ()=>{
 
+describe("BuildStep rendering", ()=>{
+
+  const details = newAttributes => Object.assign({stepId: 1, state: 'success', name: 'fooStep'}, newAttributes)
+
+  it("should render all step information", ()=>{
+    let input = details();
+
+    let component = shallow(<BuildStep buildId={1} step={input}/>);
+
+    expect(component.find('.stepName').text()).toEqual('fooStep');
+    expect(component.find('.buildStep').hasClass('success')).toBe(true);
   });
+
+  it("should render failed step state", ()=>{
+    let component = shallow(<BuildStep buildId={1} step={details({state: "failed"})}/>);
+    expect(component.find('.buildStep').hasClass('failed')).toBe(true);
+  });
+
+  it("should render running step state", ()=>{
+    let component = shallow(<BuildStep buildId={1} step={details({state: "running"})}/>);
+    expect(component.find('.buildStep').hasClass('running')).toBe(true);
+  });
+
+  it("should render pending step state", ()=>{
+    let component = shallow(<BuildStep buildId={1} step={details({state: "pending"})}/>);
+    expect(component.find('.buildStep').hasClass('pending')).toBe(true);
+  });
+
 })
