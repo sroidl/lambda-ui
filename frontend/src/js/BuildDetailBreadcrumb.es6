32ecd7d62799f47949964832cc13348e70d22212
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-16 12:07:07 +0200

    calculate breadcrumb

--- frontend/src/js/BuildDetailBreadcrumb.es6
+++ frontend/src/js/BuildDetailBreadcrumb.es6
@@ -1,4 +1,5 @@
 import React, {PropTypes} from "react";
+import * as R from "ramda";
 import "../sass/buildDetails.sass";
 
 export const BuildDetailBreadcrumb = ({steps}) => {
@@ -6,10 +7,36 @@ export const BuildDetailBreadcrumb = ({steps}) => {
     return <div className="buildDetailBreadcrumb">&gt;</div>;
   }
 
-  return null;
+  const stepsHtml = R.map(step => {
+    return <span key={step.stepName}> &gt; {step.stepName}</span>;
+  })(steps);
+
+  return <div className="buildDetailBreadcrumb">
+    {stepsHtml}
+   </div>;
 };
 
 BuildDetailBreadcrumb.propTypes = {
   steps: PropTypes.array,
   buildId: PropTypes.number.isRequired
+};
+
+const conmap = fn => (acc, child) => R.concat(acc, fn(child));
+const flatTree = stepDownFn =>
+      input => { return stepDownFn(input) ? R.reduce(conmap(flatTree(stepDownFn)),[input], stepDownFn(input)) : [input]; };
+const safeSteps = input => {return input ? input : [];};
+export const expandParents = flatTree(parent => R.map(step => Object.assign(step, {parentId: parent.stepId}), safeSteps(parent.steps)));
+
+export const calculateBreadcrumb = (buildDetails, currentViewStepId) => {
+  const allSteps = R.pipe(expandParents, R.indexBy(R.prop("stepId")));
+
+  const expandBreadcrumb = (currentId, allSteps) => {
+    const currentStep = allSteps[currentId];
+    const result = currentStep ? [expandBreadcrumb(currentStep.parentId, allSteps), currentStep] : [];
+    return R.chain(id => id, result);
+  };
+
+  const objects = R.project(["stepName", "stepId"])(expandBreadcrumb(currentViewStepId, allSteps(buildDetails)));
+
+  return objects;
 };
--- frontend/src/js/__tests__/BuildDetailBreadcrumb.test.js
+++ frontend/src/js/__tests__/BuildDetailBreadcrumb.test.js
@@ -1,11 +1,61 @@
 /* globals describe, it, expect */
-import {BuildDetailBreadcrumb as subject} from "../BuildDetailBreadcrumb.es6";
+/* eslint-disable */
+import * as subject from "../BuildDetailBreadcrumb.es6";
 import {shallow} from "enzyme";
+import * as R from "ramda";
 
 describe("Breadcrumb presentation", () => {
   it("should show only root if no step is given", () => {
 
-    expect(shallow(subject([])).find(".buildDetailBreadcrumb").text()).toEqual(">");
+    expect(shallow(subject.BuildDetailBreadcrumb([])).find(".buildDetailBreadcrumb").text()).toEqual(">");
 
   });
+
+  it("should show breadcrumb of steps in order of list", () => {
+    const steps = [{stepName: "first"}, {stepName: "Second"}];
+    expect(shallow(subject.BuildDetailBreadcrumb({steps: steps})).find(".buildDetailBreadcrumb").text()).toEqual(" > first > Second");
+  });
+
+});
+
+describe("Breadcrumb calculation", () => {
+  it("should expandParentStepId: substep of step", () => {
+    const input = {stepId: "0", steps: [{stepId: "1"}]};
+    const expected = [{stepId: "0", steps: [{stepId: "1", parentId: "0"}]}, {stepId: "1", parentId: "0"}];
+
+    expect(subject.expandParents(input)).toEqual(expected);
+  });
+
+  it("should expandParentStepId: substep of step", () => {
+    const input = {stepId: "0", steps: [{stepId: "1"}]};
+    const expected = [{stepId: "0", steps: [{stepId: "1", parentId: "0"}]}, {stepId: "1", parentId: "0"}];
+
+    expect(subject.expandParents(input)).toEqual(expected);
+  });
+
+  it("should expandParentStepId: substep of root", () => {
+    const input = {steps: [{stepId: "1"}]};
+    const expected = [{steps: [{stepId: "1", parentId: undefined}]}, {stepId: "1", parentId: undefined}];
+
+    expect(subject.expandParents(input)).toEqual(expected);
+  });
+
+  it("should calculate current breadcrumb when input is root", () => {
+    const input = {stepId: "root", stepName: "root"};
+
+    const actual = subject.calculateBreadcrumb(input, "root");
+
+    expect(actual).toEqual([{stepName: "root", stepId: "root"}]);
+
+  });
+
+  it("should calculate current breadcrumb when input is step", () => {
+    const input = {stepId: "root", stepName: "root", steps: [{stepId: "1", stepName: "substep"}]};
+
+    const actual = subject.calculateBreadcrumb(input, "1");
+
+    expect(actual).toEqual([{stepName: "root", stepId: "root"}, {stepId: "1", stepName: "substep"}]);
+
+  })
+
 });
