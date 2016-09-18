d8d8e6feb5d5f8b635000eca0b6b30d157c9a905
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-18 22:43:42 +0200

    lint test

--- frontend/src/js/__tests__/BuildStep.test.js
+++ frontend/src/js/__tests__/BuildStep.test.js
@@ -1,59 +1,58 @@
-jest.mock('../Actions.es6');
-import {viewBuildStep} from '../Actions.es6'
-import React from 'react';
-import {shallow, mount} from 'enzyme';
-import BuildStepRedux, {BuildStep} from '../BuildStep.es6'
-import {MockStore} from './TestSupport.es6'
+/* globals jest describe expect it */
+jest.mock("../Actions.es6");
+import {viewBuildStep} from "../Actions.es6";
+import React from "react";
+import {shallow, mount} from "enzyme";
+import BuildStepRedux, {BuildStep} from "../BuildStep.es6";
+import {MockStore} from "./TestSupport.es6";
 
 
-const details = newAttributes => Object.assign({stepId: 1, state: 'success', name: 'fooStep'}, newAttributes)
+const details = newAttributes => Object.assign({stepId: 1, state: "success", name: "fooStep"}, newAttributes);
 
-describe("BuildStep rendering", ()=>{
+describe("BuildStep rendering", () => {
+  it("should render all step information", () => {
+    const input = details();
 
-  it("should render all step information", ()=>{
-    let input = details();
+    const component = shallow(<BuildStep buildId={1} step={input}/>);
 
-    let component = shallow(<BuildStep buildId={1} step={input}/>);
-
-    expect(component.find('.stepName').text()).toEqual('fooStep');
-    expect(component.find('.buildStep').hasClass('success')).toBe(true);
+    expect(component.find(".stepName").text()).toEqual("fooStep");
+    expect(component.find(".buildStep").hasClass("success")).toBe(true);
   });
 
-  it("should render failed step state", ()=>{
-    let component = shallow(<BuildStep buildId={1} step={details({state: "failed"})}/>);
-    expect(component.find('.buildStep').hasClass('failed')).toBe(true);
+  it("should render failed step state", () => {
+    const component = shallow(<BuildStep buildId={1} step={details({state: "failed"})}/>);
+    expect(component.find(".buildStep").hasClass("failed")).toBe(true);
   });
 
-  it("should render running step state", ()=>{
-    let component = shallow(<BuildStep buildId={1} step={details({state: "running"})}/>);
-    expect(component.find('.buildStep').hasClass('running')).toBe(true);
+  it("should render running step state", () => {
+    const component = shallow(<BuildStep buildId={1} step={details({state: "running"})}/>);
+    expect(component.find(".buildStep").hasClass("running")).toBe(true);
   });
 
-  it("should render pending step state", ()=>{
-    let component = shallow(<BuildStep buildId={1} step={details({state: "pending"})}/>);
-    expect(component.find('.buildStep').hasClass('pending')).toBe(true);
+  it("should render pending step state", () => {
+    const component = shallow(<BuildStep buildId={1} step={details({state: "pending"})}/>);
+    expect(component.find(".buildStep").hasClass("pending")).toBe(true);
   });
 
-  it("should render link if step has substeps", ()=>{
-    let substeps = {steps: [{stepId: "1.1"}]}
+  it("should render link if step has substeps", () => {
+    const substeps = {steps: [{stepId: "1.1"}]};
 
-    let component = shallow(<BuildStep buildId={1} step={details(substeps)} />)
+    const component = shallow(<BuildStep buildId={1} step={details(substeps)} />);
 
-    expect(component.find('.goIntoStepLink').length).toBe(1);
-  })
+    expect(component.find(".goIntoStepLink").length).toBe(1);
+  });
+});
 
-})
-
-describe("BuildStep wiring", ()=> {
+describe("BuildStep wiring", () => {
   it("should dispatch go into step action on link click", () => {
-    let dispatchMock = jest.fn();
-    let storeMock = MockStore({}, dispatchMock);
-    let substeps = {steps: [{stepId: "1-1"}]}
+    const dispatchMock = jest.fn();
+    const storeMock = MockStore({}, dispatchMock);
+    const substeps = {steps: [{stepId: "1-1"}]};
     viewBuildStep.mockReturnValue({type: "stepInto"});
 
-    let component = mount(<BuildStepRedux buildId={1} step={details(substeps)} store={storeMock}/>)
-    component.find('.goIntoStepLink').simulate('click');
+    const component = mount(<BuildStepRedux buildId={1} step={details(substeps)} store={storeMock}/>);
+    component.find(".goIntoStepLink").simulate("click");
 
     expect(dispatchMock).toBeCalledWith({type: "stepInto"});
-  })
-})
+  });
+});
