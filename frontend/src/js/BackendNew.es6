228f3fb401a82333f140e5e0b18d3296ae0bb455
Sebastian Roidl <sroidl@thoughtworks.com>
2016-09-23 18:24:48 +0200

    prepare new backend implementation

--- frontend/src/js/BackendNew.es6
+++ frontend/src/js/BackendNew.es6
@@ -1,16 +1,26 @@
 /* eslint-disable */
+import * as R from "ramda";
+import {webSocket} from "./WebSocketFactory.es6";
 
+const outputUrl = (baseUrl, buildId, stepId) => "ws://" + baseUrl + "/builds/" + buildId + "/" + stepId;
 
 export class Backend {
     constructor({baseUrl}) {
         this.baseUrl = baseUrl;
+        this.outputUrl = R.curry(outputUrl)(baseUrl);
+    }
+
+    _closeConnection(websocket) {
+      const CLOSED = 3;
+      if (websocket && websocket.readystate !== CLOSED) {
+        websocket.close();
+      }
     }
 
     requestOutput(dispatch, buildId, stepId) {
+      this._closeConnection(this.outputConnection);
+      this.outputConnection = webSocket(this.outputUrl(buildId, stepId));
 
     };
 
 }
-
-
-
--- frontend/src/js/__tests__/Backend.test.js
+++ frontend/src/js/__tests__/Backend.test.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-/* globals describe it jest expect */
+/* globals describe it jest expect beforeEach */
 jest.mock("../WebSocketFactory.es6");
 import {Backend} from "../BackendNew.es6";
 import {webSocket} from "../WebSocketFactory.es6";
@@ -8,27 +7,37 @@ const _it = () => {};
 
 describe("Backend: output connection", () => {
     _it("should close websocket if output view changes", () => {
-        const webSocketMock = {readystate: 3, close: jest.fn()};
-        webSocket.mockReturnValue(webSocketMock);
-        const requestOutput = subject.outputConnection.requestOutput(jest.fn(), "url");
-
-        requestOutput(1, 1);
-        requestOutput(1, 2);
-
-        expect(webSocketMock.close).toBeCalled();
+        // const webSocketMock = {readystate: 3, close: jest.fn()};
+        // webSocket.mockReturnValue(webSocketMock);
+        // const requestOutput = subject.outputConnection.requestOutput(jest.fn(), "url");
+        //
+        // requestOutput(1, 1);
+        // requestOutput(1, 2);
+        //
+        // expect(webSocketMock.close).toBeCalled();
     });
 });
 
 
 describe("New Backend: Configuration", () => {
-    it("should extract baseUrl", () => {
-        const subject = new Backend({baseUrl: "baseUrl"});
+    let subject;
+    let dispatchMock;
+    let websocketMock;
 
+    beforeEach(() => {
+      subject = new Backend({baseUrl: "baseUrl"});
+      dispatchMock = jest.fn();
+      websocketMock = {close : jest.fn()};
+      webSocket.mockReturnValue = websocketMock;
+    });
+
+    it("should extract baseUrl", () => {
         expect(subject.baseUrl).toBe("baseUrl");
     });
 
     it("should request output", () => {
+        subject.requestOutput(dispatchMock, 1, 2);
 
+        expect(webSocket).toBeCalledWith("ws://baseUrl/builds/1/2");
     });
 });
-
