2c01f3392fb7b467f3d77e3b3149799d30995514
Sebastian Roidl <sroidl@thoughtworks.com>
2016-10-07 14:54:59 +0200

    refactored test subject reference name

--- backend/test/lambdaui/api_test.clj
+++ backend/test/lambdaui/api_test.clj
@@ -1,6 +1,6 @@
 (ns lambdaui.api-test
   (:require [clojure.test :refer :all]
-            [lambdaui.api :as api]
+            [lambdaui.api :as subject]
             [lambdacd.steps.control-flow :as ctrl-flow]
             [lambdacd.event-bus :as event-bus]
             [lambdacd.internal.pipeline-state :as state]
@@ -20,13 +20,13 @@
                            :startTime   nil
                            :endTime     nil
                            }]}
-             (api/summaries simple-waiting-pipeline-state)))
+             (subject/summaries simple-waiting-pipeline-state)))
       (is (= {:summaries [{:buildNumber 1
                            :buildId     1
                            :state       :running
                            :startTime   nil
                            :endTime     nil}]}
-             (api/summaries simple-running-pipeline-state)))))
+             (subject/summaries simple-running-pipeline-state)))))
   (testing "should extract pipeline summaries for more than one pipeline"
     (let [waiting-and-running-pipeline-state {1 {'(1) {:status :waiting}}
                                               2 {'(1) {:status :running}}}]
@@ -40,7 +40,7 @@
                            :state       :running
                            :startTime   nil
                            :endTime     nil}]}
-             (api/summaries waiting-and-running-pipeline-state))))))
+             (subject/summaries waiting-and-running-pipeline-state))))))
 
 (deftest extract-state-test
   (testing "should be waiting if some step is waiting"
@@ -50,8 +50,8 @@
           multi-step-build2 {'(1) {:status :success}
                              '(2) {:status :waiting}
                              '(3) {:status :running}}]
-      (is (= :waiting (api/extract-state multi-step-build1)))
-      (is (= :waiting (api/extract-state multi-step-build2)))))
+      (is (= :waiting (subject/extract-state multi-step-build1)))
+      (is (= :waiting (subject/extract-state multi-step-build2)))))
   (testing "should be running if some step is running but none waiting"
     (let [multi-step-build1 {'(1) {:status :success}
                              '(2) {:status :running}
@@ -59,8 +59,8 @@
           multi-step-build2 {'(1) {:status :success}
                              '(2) {:status :running}
                              '(3) {:status :success}}]
-      (is (= :running (api/extract-state multi-step-build1)))
-      (is (= :running (api/extract-state multi-step-build2)))))
+      (is (= :running (subject/extract-state multi-step-build1)))
+      (is (= :running (subject/extract-state multi-step-build2)))))
   (testing "should be failure if last step is failure"
     (let [multi-step-build1 {'(1) {:status :success}
                              '(2) {:status :success}
@@ -68,8 +68,8 @@
           multi-step-build2 {'(1) {:status :success}
                              '(2) {:status :failure}
                              '(3) {:status :running}}]
-      (is (= :failed (api/extract-state multi-step-build1)))
-      (is (= :running (api/extract-state multi-step-build2)))))
+      (is (= :failed (subject/extract-state multi-step-build1)))
+      (is (= :running (subject/extract-state multi-step-build2)))))
   (testing "should be success if last step is success and no step is running or waiting"
     (let [multi-step-build1 {'(1) {:status :success}
                              '(2) {:status :success}
@@ -80,9 +80,9 @@
           multi-step-build3 {'(1) {:status :running}
                              '(2) {:status :failure}
                              '(3) {:status :success}}]
-      (is (= :success (api/extract-state multi-step-build1)))
-      (is (= :success (api/extract-state multi-step-build2)))
-      (is (= :running (api/extract-state multi-step-build3))))))
+      (is (= :success (subject/extract-state multi-step-build1)))
+      (is (= :success (subject/extract-state multi-step-build2)))
+      (is (= :running (subject/extract-state multi-step-build3))))))
 
 (deftest extract-start-time-test
   (testing "should extract time from single step build"
@@ -90,7 +90,7 @@
           joda-date-14      (DateTime. 2016 01 01 14 00 (DateTimeZone/UTC))
           single-step-build {'(1) {:first-updated-at      joda-date-12
                                    :most-recent-update-at joda-date-14}}]
-      (is (= "2016-01-01T12:00:00.000Z" (api/extract-start-time single-step-build)))))
+      (is (= "2016-01-01T12:00:00.000Z" (subject/extract-start-time single-step-build)))))
   (testing "should take time of first step from multi step build"
     (let [joda-date-12     (DateTime. 2016 01 01 12 00 (DateTimeZone/UTC))
           joda-date-14     (DateTime. 2016 01 01 14 00 (DateTimeZone/UTC))
@@ -100,10 +100,10 @@
                                     :most-recent-update-at joda-date-14}
                             '(1 1) {:first-updated-at      joda-date-15
                                     :most-recent-update-at joda-date-16}}]
-      (is (= "2016-01-01T12:00:00.000Z" (api/extract-start-time multi-step-build)))))
+      (is (= "2016-01-01T12:00:00.000Z" (subject/extract-start-time multi-step-build)))))
   (testing "should return nil if no time found"
     (let [single-empty-step {'(1) {}}]
-      (is (= nil (api/extract-start-time single-empty-step))))))
+      (is (= nil (subject/extract-start-time single-empty-step))))))
 
 (deftest extract-end-time-test
   (testing "should extract time from single step build"
@@ -111,7 +111,7 @@
           joda-date-14      (DateTime. 2016 01 01 14 00 (DateTimeZone/UTC))
           single-step-build {'(1) {:first-updated-at      joda-date-12
                                    :most-recent-update-at joda-date-14}}]
-      (is (= "2016-01-01T14:00:00.000Z" (api/extract-end-time single-step-build)))))
+      (is (= "2016-01-01T14:00:00.000Z" (subject/extract-end-time single-step-build)))))
   (testing "should take latest available time from multi-step build"
     (let [joda-date-12     (DateTime. 2016 01 01 12 00 (DateTimeZone/UTC))
           joda-date-14     (DateTime. 2016 01 01 14 00 (DateTimeZone/UTC))
@@ -124,15 +124,14 @@
                             '(2)   {:first-updated-at      joda-date-12
                                     :most-recent-update-at joda-date-14}
                             }]
-      (is (= "2016-01-01T16:00:00.000Z" (api/extract-end-time multi-step-build)))))
+      (is (= "2016-01-01T16:00:00.000Z" (subject/extract-end-time multi-step-build)))))
   (testing "should return nil if no time found"
     (let [single-empty-step {'(1) {}}]
-      (is (= nil (api/extract-end-time single-empty-step))))))
+      (is (= nil (subject/extract-end-time single-empty-step))))))
 
 
 
 (defn do-stuff [] {})
-(defn do-other-stuff [] {})
 
 (def foo-pipeline
   `(do-stuff))
@@ -170,7 +169,7 @@
                                   :name      "do-stuff"
                                   :startTime "2016-01-01T12:00:00.000Z"
                                   :endTime   nil}]}
-                      (api/build-details-from-pipeline foo-pipeline (foo-pipeline-build-state running-status) buildId)))))))
+                      (subject/build-details-from-pipeline foo-pipeline (foo-pipeline-build-state running-status) buildId)))))))
   (testing "that it returns build details of a finished step"
     (doall (for [finished-status [:success :failure :killed]]
              (let [buildId 1]
@@ -180,7 +179,7 @@
                                   :name      "do-stuff"
                                   :startTime "2016-01-01T12:00:00.000Z"
                                   :endTime   "2016-01-01T14:00:00.000Z"}]}
-                      (api/build-details-from-pipeline foo-pipeline (foo-pipeline-build-state finished-status) buildId)))))))
+                      (subject/build-details-from-pipeline foo-pipeline (foo-pipeline-build-state finished-status) buildId)))))))
   (testing "that it returns build details"
     (let [buildId 1]
       (is (= {:buildId 1
@@ -199,7 +198,7 @@
                                       :state     :pending
                                       :name      "do-stuff"
                                       :startTime nil
-                                      :endTime   nil}]}]} (api/build-details-from-pipeline pipeline-with-substeps pipeline-with-substeps-state buildId))))))
+                                      :endTime   nil}]}]} (subject/build-details-from-pipeline pipeline-with-substeps pipeline-with-substeps-state buildId))))))
 
 (deftest output-websocket
   (testing "that a payload is sent through the ws channel"
@@ -211,7 +210,7 @@
       (with-redefs [event-bus/subscribe (fn [ctx topic] nil)
                     event-bus/only-payload (fn [subscription] event-channel)
                     state/get-all (fn [_] {})]
-        (api/build-step-events-to-ws nil ws-ch 1 "2-1")
+        (subject/build-step-events-to-ws nil ws-ch 1 "2-1")
         (async/>!! event-channel {:build-number 1 :step-id [2 1] :step-result {:foo :bar}})
         (async/<!! sent-channel) ;ignore first msg. not part of test
         (is (= (json/write-str {:stepId "2-1" :buildId 1 :stepResult {:foo :bar}}) (async/<!! sent-channel)))))))
@@ -221,5 +220,10 @@
     (let [in-ch (async/to-chan [{:build-number 1 :step-id [2 1] :step-result {:foo :bar}}
                                 {:build-number 2 :step-id [2 1] :step-result {:foo :bar}}
                                 {:build-number 1 :step-id [3 1] :step-result {:foo :bar}}])
-          out-ch (api/only-matching-step in-ch 1 "2-1")]
+          out-ch (subject/only-matching-step in-ch 1 "2-1")]
       (is (= [{:buildId 1 :stepId "2-1" :stepResult {:foo :bar}}] (async/<!! (async/into [] out-ch)))))))
+
+(deftest details-websocket
+  (testing "that a payload is sent through the ws channel"
+
+    ))
