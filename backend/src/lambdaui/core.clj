ab00e635b0d3c8a2a6608ecb75a0a0b3e6403293
Sebastian Roidl <sebastian.roidl@otto.de>
2016-10-21 16:34:36 +0200

    add trigger new

--- backend/src/lambdaui/core.clj
+++ backend/src/lambdaui/core.clj
@@ -1,6 +1,6 @@
 (ns lambdaui.core
   (:require
-    [compojure.core :refer [routes GET context]]
+    [compojure.core :refer [routes GET context POST]]
     [lambdaui.api :as new-api]
     [lambdacd-git.core :as git]
     [lambdacd.core :as lambdacd]
@@ -14,6 +14,15 @@
     [compojure.route :as route])
   (:gen-class)
   )
+
+
+(defn get-trigger-id [pipeline]
+  (((val (last (lambdacd.internal.pipeline-state/get-all (get-in pipeline [:context :pipeline-state-component])))) '(1 1)) :trigger-id)
+  )
+
+(defn trigger-new [pipeline]
+  (let [ctx (:context pipeline)]
+    (manualtrigger/post-id ctx (get-trigger-id pipeline) {})))
 
 (defn try-parse-int [port default-fn]
   (try (Integer/parseInt port) (catch NumberFormatException e (default-fn e))))
@@ -31,6 +40,7 @@
 (defn pipeline-routes [pipeline]
   (ring-json/wrap-json-response
     (routes (context "/lambdaui/api" [] (new-api/api-routes pipeline))
+            (POST "/lambdaui/api/triggerNew" [] (do (trigger-new pipeline) {}))
             (context "/api" [] (old-api/rest-api pipeline))
             (GET "/old" [] (old-ui/ui-page pipeline))
             (route/resources "/" {:root "public"})
@@ -39,12 +49,9 @@
             (route/resources "/ui" {:root "public/target"})
             )))
 
-(defonce pipe (atom nil))
-
 (defn start-server [port]
   (git/init-ssh!)
   (let [pipeline (lambdacd/assemble-pipeline pipe/pipeline-structure {:home-dir "tmp/foo"})]
-    (reset! pipe pipeline)
     (runners/start-one-run-after-another pipeline)
     (reset! server (http/run-server (pipeline-routes pipeline) {:port port}))))
 
@@ -53,10 +60,3 @@
     (println "Started Server on port " port ". CTRL+C to abort.")
     (start-server port)))
 
-(defn get-trigger-id []
-  (((val (last (lambdacd.internal.pipeline-state/get-all (get-in @pipe [:context :pipeline-state-component])))) '(1 1)) :trigger-id)
-  )
-
-(defn trigger-new []
-  (let [ctx (:context @pipe)]
-    (manualtrigger/post-id ctx (get-trigger-id) {})))
