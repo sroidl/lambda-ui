ad21ff07aae88af44b22328c8b9c731af2954f9f
Sebastian Roidl <sebastian.roidl@otto.de>
2016-11-25 16:42:17 +0100

    prepare build trigger for specific git hash

--- backend/src/lambdaui/core.clj
+++ backend/src/lambdaui/core.clj
@@ -3,6 +3,7 @@
     [compojure.core :refer [routes GET context POST]]
     [lambdaui.api :as new-api]
     [ring.middleware.json :as ring-json]
+    [ring.middleware.params :refer [wrap-params]]
     [lambdacd.ui.api :as old-api]
     [lambdacd.ui.ui-page :as old-ui]
     [compojure.route :as route]
@@ -33,15 +34,16 @@
 
 (defn pipeline-routes [pipeline]
   (ring-json/wrap-json-response
-    (routes
-      (route/resources "/lambdaui" {:root "public"})
-      (route/resources "/" {:root "public"})
-      (GET "/lambdaui" [] (ring.util.response/redirect "lambdaui/index.html"))
-      (GET "/lambdaui/config.js" [] (create-config pipeline))
-      (context "/lambdaui/api" [] (new-api/api-routes pipeline))
-      (POST "/lambdaui/api/triggerNew" [] (do (runner/trigger-new-build pipeline) {}))
-      (GET "/" [] (old-ui/ui-page pipeline))
-      (context "/api" [] (old-api/rest-api pipeline))
-      )))
+    (wrap-params
+      (routes
+        (route/resources "/lambdaui" {:root "public"})
+        (route/resources "/" {:root "public"})
+        (GET "/lambdaui" [] (ring.util.response/redirect "lambdaui/index.html"))
+        (GET "/lambdaui/config.js" [] (create-config pipeline))
+        (context "/lambdaui/api" [] (new-api/api-routes pipeline))
+        (POST "/lambdaui/api/triggerNew" request (do (runner/trigger-new-build pipeline request) {}))
+        (GET "/" [] (old-ui/ui-page pipeline))
+        (context "/api" [] (old-api/rest-api pipeline))
+        ))))
 
 
--- backend/src/lambdaui/trigger.clj
+++ backend/src/lambdaui/trigger.clj
@@ -1,10 +1,17 @@
 (ns lambdaui.trigger
   (:require [clojure.core.async :as async]
-            [lambdacd.internal.execution :as execution]))
+            [lambdacd.internal.execution :as execution]
+            [clojure.walk :refer [keywordize-keys]]))
 
 
-(defn trigger-new-build [{pipeline-def :pipeline-def context :context}]
+;{:post [(v/is-not-nil-or-empty? (:branch (:global %)))
+;        (v/is-not-nil-or-empty? (:revision (:global %)))
+;        (v/is-valid-revision? (:revision (:global %)))]}
+
+(defn trigger-new-build [{pipeline-def :pipeline-def context :context} {query-param :query-params}]
   (async/thread
-    (println "Starting new build")
+
+    ;(println "Starting new build " )
+    ;(when-let [git-commit (:git (keywordize-keys query-param))] (println "requested commit was" git-commit))
     (execution/run pipeline-def context)
     ))
