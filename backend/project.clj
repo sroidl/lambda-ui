7633606b770e397ffb3dbf8cdd86b12bfd4a0a31
Marco Geweke <marco.geweke@otto.de>
2017-03-24 09:57:40 +0100

    lein-release plugin: bumped version from 0.3.4 to 0.3.5-SNAPSHOT for next development cycle

--- backend/project.clj
+++ backend/project.clj
@@ -1,4 +1,4 @@
-(defproject lambdaui "0.3.4"
+(defproject lambdaui "0.3.5-SNAPSHOT"
   :description "LambdaCD-Plugin that provides a modern UI for your pipeline."
   :url "https://github.com/sroidl/lambda-ui"
   :license {:name "Apache License 2.0"
