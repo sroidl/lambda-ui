c70f3bb68e1b178dc542f564333d3da19c4fa3dd
Florian Sellmayr <florian.sellmayr@gmail.com>
2018-09-01 14:30:47 +0700

    Bump http-kit dependency to make lambda-ui work on recent Java versions

--- backend/project.clj
+++ backend/project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [lambdacd "0.13.3"]
                  [compojure "1.6.0"]
-                 [http-kit "2.2.0"]
+                 [http-kit "2.3.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.slf4j/slf4j-simple "1.7.25"]
                  [trptcolin/versioneer "0.2.0"]]
