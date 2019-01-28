ca3c7f31686b2141ecdff904d056c6f2c27d8b7d
Sebastian Roidl <sroidl@thoughtworks.com>
2019-01-29 00:01:57 +0100

    Version 1.2.1-SNAPSHOT

--- backend/project.clj
+++ backend/project.clj
@@ -1,4 +1,4 @@
-(defproject lambdaui "1.2.0"
+(defproject lambdaui "1.2.1-SNAPSHOT"
   :description "LambdaCD-Plugin that provides a modern UI for your pipeline."
   :url "https://github.com/sroidl/lambda-ui"
   :license {:name "Apache License 2.0"
