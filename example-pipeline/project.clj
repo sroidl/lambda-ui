192923b0658db81a2c0a1b30ac09c1d3fb01dfa7
Sebastian Roidl <sroidl@thoughtworks.com>
2019-01-28 23:25:33 +0100

    prepare release of 1.2.0

--- example-pipeline/project.clj
+++ example-pipeline/project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [lambdaui "1.0.1-SNAPSHOT"]
-                 [lambdacd "0.14.0"]
-                 [lambdacd-git "0.2.0"]
-                 [http-kit "2.1.18"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [lambdaui "1.2.0-SNAPSHOT"]
+                 [lambdacd "0.14.3"]
+                 [lambdacd-git "0.4.1"]
+                 [http-kit "2.3.0"]]
   :main ^:skip-aot lambdaui.example.simple-pipeline
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.8.3"
 
 
 
